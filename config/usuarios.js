a9293355e4cac8228e1d724363305915a19e35e9
robertocolman <robertocolman93@gmail.com>
2025-10-16 11:21:34 -0300

    se agregó login

---
+++ config/usuarios.js
@@ -0,0 +1,10 @@
+const usuarios = [
+    {
+        usuario: "admin",
+        clave: "1234"
+    },
+    {
+        usuario: "Colman",
+        clave: "123456"
+    }
+];
---
+++ js/login.js
@@ -0,0 +1,31 @@
+const formLogin = document.getElementById('formLogin');
+const usuario = document.getElementById('usuario');
+const clave = document.getElementById('clave');
+const mensaje = document.getElementById('mensaje');
+
+function mostrarMensaje(texto, tipo) {
+    mensaje.innerHTML = `
+        <div class="col-md-6 col-lg-4">
+            <div class="alert alert-${tipo}">${texto}</div>
+        </div>
+    `;
+}
+
+formLogin.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let usuarioInput = usuario.value.trim();
+    let claveInput = clave.value.trim();
+
+    const isUsuario = usuarios.find(
+        u => u.usuario === usuarioInput && u.clave === claveInput
+    );
+
+    if(isUsuario) {
+        sessionStorage.setItem("usuarioLogueado", usuarioInput);
+        mostrarMensaje(`Bienvenido Usuario ${usuarioInput}`, "success");
+        //window.location.href = "altaMedicos.html";
+    } else {
+        mostrarMensaje('Error en credenciales', "danger")
+    }
+});
